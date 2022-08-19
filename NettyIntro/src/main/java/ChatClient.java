ae24283ccff44e35e12845f5f331be0739694482
charan2000 <charanbhavisetti2000@gmail.com>
2022-08-19 19:59:56 +0530

    Created Client Chat

--- NettyIntro/src/main/java/ChatClient.java
+++ NettyIntro/src/main/java/ChatClient.java
@@ -1,5 +1,13 @@
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ChatClient {
 
@@ -14,6 +22,24 @@ public class ChatClient {
 
     public void run() {
         EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap bootstrap = new Bootstrap().group(group).channel(NioSocketChannel.class).handler(new ChatClientInitializer());
+            Channel channel = bootstrap.connect(host,port).sync().channel();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                channel.write("C: "+br.readLine() + "\n ");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatClient("localhost",9999).run();
     }
 
 }
