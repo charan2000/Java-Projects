2919b15ca1676294063d747c1e419a63d1005d3e
charan2000 <charanbhavisetti2000@gmail.com>
2022-09-08 12:28:00 +0530

    Completed!

---
+++ TaskOneTripsAnalytics/src/trips/analytics/ConvertionLatLonToKM.java
@@ -0,0 +1,20 @@
+package trips.analytics;
+
+class ConvertionLatLonToKM {
+
+    protected static int latLonToKM(double eLat,double eLon, double sLat, double sLon) {
+
+        int radius = 6371;
+        double dLat = (eLat-sLat) * (Math.PI/180);
+        double dLon = (eLon-sLon) * (Math.PI/180);
+
+//      Haversine method to Calculate Distance using Latitide and Longitude Values
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2)
+                + Math.cos((sLat) * (Math.PI/180)) * Math.cos((eLat)*(Math.PI/180))
+                * Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        int distance = (int) Math.round(radius * c);
+        return distance;
+
+    }
+}
--- TaskOneTripsAnalytics/src/trips/analytics/TripsAnalytics.java
+++ TaskOneTripsAnalytics/src/trips/analytics/TripsAnalytics.java
@@ -1,4 +1,4 @@
-package trips.anapytics;
+package trips.analytics;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -6,10 +6,12 @@ import java.util.List;
 
 public class TripsAnalytics {
 
-    private static final String path = "/home/charan/trips_view.csv";
-    private static final String testPath = "/home/charan/TaskOneTripJumps.csv";
+//    Paths for file Input & Output
+    private static final String path = "src/trips_view.csv";
+    private static final String testPath = "src/TaskOneTripJumps.csv";
     private static String line = "";
 
+    // Method That gets the end Lat & Lon Values and the consecutive Start Lat & Lon values
     private static List<Double> getValuesOfLatLon(int num, String pos) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader(path));
@@ -45,32 +47,23 @@ public class TripsAnalytics {
         String newColumn = "";
         List<Double> startValues;
         List<Double> endValues;
-        int distance = 0;
-
+        int distance;
         int count = 0;
+
         while ((line = br.readLine()) != null) {
             String existingLine = line;
             if(count==0) {
             System.out.println(existingLine);
                 bw.write(existingLine+","+"jumpedDistance"+System.getProperty("line.separator"));
-//                count++;
             }
-            if (count > 1) {
 
+            if (count > 1) {
                 startValues = getValuesOfLatLon(count, "start");
                 endValues = getValuesOfLatLon(count - 1, "end");
-//                System.out.println(startValues);
-//                System.out.println(" ");
-//                System.out.println(endValues);
                 distance = (ConvertionLatLonToKM.latLonToKM(endValues.get(0),endValues.get(1),startValues.get(0),startValues.get(1)));
 
+                // IF statement to get only distance > 5KM
                 if (distance > 5) {
-//                    System.out.println("\nStart Values");
-//                    System.out.println(startValues);
-//                    System.out.println("End Values");
-//                    System.out.println(endValues);
-//                    System.out.print("Distance is: ");
-//                    System.out.print(distance);
                     newColumn = existingLine + ","+distance+System.getProperty("line.separator");
                     System.out.println(newColumn);
                     try{
@@ -79,7 +72,6 @@ public class TripsAnalytics {
                         catch (Exception e) {
                             e.printStackTrace();
                         }
-//            System.out.println(line);
                     }
                 }
                 count++;
